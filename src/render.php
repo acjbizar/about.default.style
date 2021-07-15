49559670e274d94d904afa719d531a3bf708ce07
ACJ <gitlab@acjs.net>
2021-07-15 18:38:07 +0200

    Add Markdown and Prism and whatnot

--- src/render.php
+++ src/render.php
@@ -3,11 +3,24 @@
 require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
 
 use Twig\Environment;
-use Twig\Loader\ArrayLoader;
+use Twig\Extra\Markdown\MarkdownExtension;
+use Twig\Extra\Markdown\DefaultMarkdown;
+use Twig\Extra\Markdown\MarkdownRuntime;
+use Twig\RuntimeLoader\RuntimeLoaderInterface;
 use Twig\Loader\FilesystemLoader;
 
 $loader = new FilesystemLoader('templates');
 $twig = new Environment($loader);
+$twig->addExtension(new MarkdownExtension());
+
+$twig->addRuntimeLoader(new class implements RuntimeLoaderInterface {
+    public function load($class): MarkdownRuntime
+    {
+        if (MarkdownRuntime::class === $class) {
+            return new MarkdownRuntime(new DefaultMarkdown());
+        }
+    }
+});
 
 $r = $twig->render('index.html.twig', ['name' => 'Fabien']);
 
